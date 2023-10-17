ef7fcaaefee51b44e26a21937d32f85f3cc24d95
Betul Arpinar <betularpinar@macbook-pro-102.lan>
2023-10-16 21:30:31 -0400

    added an image upload field

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
@@ -24,6 +24,7 @@ export default function PostCard() {
   const [tagged, setTagged] = useState('');
   const [microchipped, setMicrochipped] = useState('');
   const [spayed, setSpayed] = useState('');
+  const [image, setImage] = useState('');
   
   const sizes = [
     {
@@ -65,6 +66,12 @@ export default function PostCard() {
       spayed: spayed,
     });
   }
+
+  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+   }
 
   return (
     <Card sx={{ width: 500 }}>
@@ -172,6 +179,24 @@ export default function PostCard() {
                 <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
                 <FormControlLabel value="No" control={<Radio />} label="No" />
               </RadioGroup>
+
+              <FormLabel id="spayed-label">Upload a picture of your pet</FormLabel>
+              <div style={{marginTop: 4}}>
+                  <input type="file" onChange={onImageChange} className="filetype" />
+                  {image && (<Box
+                    component="img"
+                    sx={{
+                      height: 233,
+                      width: 350,
+                      marginTop: 2,
+                      maxHeight: { xs: 233, md: 167 },
+                      maxWidth: { xs: 350, md: 250 },
+                    }}
+                    alt="The photo of the pet"
+                    src={image}
+                  />)}
+
+              </div>
             </FormControl>
           </Box>
 
