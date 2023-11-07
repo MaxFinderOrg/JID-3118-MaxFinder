5174919ce977f23d3cee830e841a320c4a5ecc9f
Maddy Jaime <maddykjaime12@gmail.com>
2023-11-06 21:06:29 -0500

    getting clicked address, state, zip, etc

--- src/components/Post/Map2.tsx
+++ src/components/Post/Map2.tsx
@@ -1,23 +1,46 @@
 
-
 import React, { useEffect, useState } from "react";
-import GoogleMapReact from "google-map-react";
+import GoogleMapReact from "google-map-react"; 
+/*import Marker from "google-map-react"; */
+import {fromLatLng } from "react-geocode";
+  
 
-
+const Marker = ({ lat, lng } : { lat: number, lng: number }) => (
+    <div
+      style={{
+        width: "20px",
+        height: "20px",
+        backgroundColor: "red",
+        borderRadius: "50%",
+        border: "2px solid white",
+      }}
+    />
+  );
 
 
 export default function Map() {
     const mapStyles = {
         height: "400px",
-        width: "400px"
+        width: "400px",
+        "paddingTop": "20px",
+        "paddingBottom": "60px",
+       
     };
 
-    // Define the type for userLocation
     const initialUserLocation = { lat: 0, lng: 0 };
     const [userLocation, setUserLocation] = useState<{ lat: number; lng: number }>(initialUserLocation);
+    const [markerLocation, setMarkerLocation] = useState<{ lat: number; lng: number } | null>(null);
+
+
+    const [address, setAddress] = useState<string | null>(null);
+    const [country, setCountry] = useState<string | null>(null);
+    const [state, setState] = useState<string | null>(null);
+    const [city, setCity] = useState<string | null>(null);
+   
+
+
 
     useEffect(() => {
-        // Use the Geolocation API to get the user's current location
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition((position) => {
                 const { latitude, longitude } = position.coords;
@@ -26,37 +49,89 @@ export default function Map() {
         }
     }, []);
 
+    const handleMapClick = ({ lat, lng, event }: { lat: number, lng: number, event: any }) => {
+        // Update the markerLocation state with the clicked coordinates
+        console.log(`handleMapClick(): (${lat}, ${lng})`);
+        setMarkerLocation({ lat, lng });
+
+
+        const myAPIKey = "d6b32867b992488091820bcca116a039";
+        const reverseGeocodingUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&format=json&&apiKey=${myAPIKey}`;
+  
+       // call Reverse Geocoding API - https://www.geoapify.com/reverse-geocoding-api/
+       fetch(reverseGeocodingUrl)
+       .then((response) => {
+         if (!response.ok) {
+           throw new Error("Network response was not ok");
+         }
+         return response.json();
+       })
+       .then((featureCollection) => {
+         
+        console.log("feature collection:")
+        console.log(featureCollection);
+        //const foundAddress = featureCollection.features[0];
+        const selectedJSON = featureCollection.results[0];
+
+        console.log("selectedJSON:")
+        console.log(selectedJSON);
+
+        setAddress(selectedJSON.formatted);
+        console.log(`address: ${address}`)
+
+
+        setCountry(selectedJSON.country);
+        console.log(`country: ${country}`)
+
+        setState(selectedJSON.state);
+        console.log(`state: ${state}`)
+
+
+        setCity(selectedJSON.city);
+        console.log(`city: ${city}`)
+     
+         
+       });
+     
+     
+     
+     
+     
+
+        const element = document.getElementById("selectedAddress");
+        if (element) {
+            element.innerHTML = `Selected Address: (${address})`;
+        }
+
+
+
+
+    };
+
     return (
         <div>
-            <h4>
-                See Map2 Below
-            </h4>
+            <h4>See Map2 Below</h4>
             <div style={mapStyles}>
                 <GoogleMapReact
                     bootstrapURLKeys={{
-                        key: "AIzaSyDpq2dnOVnsEeUdBPvQufmvDNF6Znm8_eM" // Replace with your Google Maps API key
+                        key: "AIzaSyDpq2dnOVnsEeUdBPvQufmvDNF6Znm8_eM"
                     }}
                     center={userLocation}
-                    defaultZoom={15} // You can adjust the zoom level as needed
+                    defaultZoom={17}  
+                    onClick={handleMapClick}
                 >
-                    {/* Add map markers, polygons, or other map components here */}
+                    {/* Display the marker if markerLocation is set */}
+                    {markerLocation && (
+                        <Marker lat={markerLocation.lat} lng={markerLocation.lng} />
+                    )}
                 </GoogleMapReact>
+
+            <h6 id = 'selectedAddress'> Click on map to select an address </h6>
+
             </div>
-            
         </div>
     );
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
